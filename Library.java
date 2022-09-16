178c0fc64d9261bb69dd989dc67ce4b5bcfb0881
Nipuni Wimangsa <57351855+Nipuni-Wimangsa@users.noreply.github.com>
2022-09-16 07:44:48 +0530

    Create Library.java

---
+++ Library.java
@@ -0,0 +1,13 @@
+public class Library {
+    //to distinguishably identify the libraries from each other
+    private String libraryName;
+    private String address;
+
+    public Library(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+}
